fb827cc485d063cfa32c276f62de9a10b7f63696
SAIFZaman007 <saifuzzamanspark@gmail.com>
2026-03-14 10:11:13 +0600

    REFINED

--- prisma/migrations/20260312133204_add_card_receive_bank/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `cardReceiveBack` on the `card_sharing` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "card_sharing" DROP COLUMN "cardReceiveBack",
-ADD COLUMN     "cardReceiveBank" TEXT NOT NULL DEFAULT '';
